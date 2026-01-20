65c1929b2ffa18885814192cde78614fcb238b8b
SEBK4C <SEBK4C@github.com>
2026-01-20 16:35:20 +0200

    Update README.md to include additional details on project milestones and user instructions for the wilderness expedition planning tool.

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ProjectTrackerDashboard from '../outputs/project-tracker-dashboard.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ProjectTrackerDashboard />
+  </React.StrictMode>,
+)
